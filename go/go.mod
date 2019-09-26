3a9ecbae77ec4c64624459d3fdbeaad7323a8c35
Li Lin <guru.lin@gmail.com>
2019-09-26 15:55:52 +0800

    update

--- go/go.mod
+++ go/go.mod
@@ -2,4 +2,4 @@ module github.com/sloppycoder/ydbtst
 
 go 1.13
 
-require lang.yottadb.com/go/yottadb v1.0.0 // indirect
+require lang.yottadb.com/go/yottadb v1.0.0
--- go/main.go
+++ go/main.go
@@ -8,7 +8,6 @@ import (
 
 const (
 	Global = "^acc"
-	Id = "10001"
 )
 
 func main() {
@@ -17,14 +16,17 @@ func main() {
 	var errstr yottadb.BufferT
 
 	tptoken := yottadb.NOTTP
-	err := yottadb.SetValE(tptoken, &errstr, "300.00", Global, []string {Id})
-	if err != nil {
-		log.Fatalf("error during save %v, errstr %v", err.Error(), errstr)
-	}
+	for i := 1; i < 1000; i++ {
+		id := fmt.Sprintf("%05d", i)
+		err := yottadb.SetValE(tptoken, &errstr, "300.00", Global, []string{id})
+		if err != nil {
+			log.Fatalf("error during save %v, errstr %v", err.Error(), errstr)
+		}
 
-	val, err := yottadb.ValE(tptoken, &errstr, Global, []string{Id})
-	if err != nil {
-		log.Fatalf("err=%v, errstr=%s", err.Error(), errstr)
+		val, err := yottadb.ValE(tptoken, &errstr, Global, []string{id})
+		if err != nil {
+			log.Fatalf("err during read %v, errstr %v", err.Error(), errstr)
+		}
+		fmt.Printf("===>%s\n", val)
 	}
-	fmt.Printf("===>%s\n",val)
 }
